9e6081eae9e95f12f672cb09ef599781c5808f80
Super-T02 <75214791+Super-T02@users.noreply.github.com>
2021-09-02 17:30:52 +0200

    TW-2: first concept of naviagtion

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 
 // Style imports
 import './index.scss';
+import style from './index.module.scss'
 
 // Internal imports
 import './api/weather/weather';
@@ -32,13 +33,65 @@ class Content extends React.Component {
 }
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            active: 'Wetter',
+            title: 'Wetter',
+        }
+    }
+
+    changeElement(clicked){
+        if (clicked !== this.state.active){
+            this.setState({active: clicked, title: clicked});
+        }
+    }
 
     render() {
-        const title = 'Wetter';
+        const {title, active} = this.state;
+        let elements = [];
+
+        if(active === 'Home') elements.push(
+            <div
+                className={style.navElement + ' ' + style.active}
+                onClick={() => this.changeElement('Home')}>
+                Home
+            </div>);
+        else elements.push(
+            <div
+                className={style.navElement}
+                onClick={() => this.changeElement('Home')}>
+                Home
+            </div>
+        );
+
+        if (active === 'Wetter') elements.push(
+            <div
+                className={style.navElement+ ' ' + style.active}
+                onClick={() => this.changeElement('Wetter')}>
+                Wetter
+            </div>
+        );
+        else elements.push(
+            <div
+                className={style.navElement}
+                onClick={() => this.changeElement('Wetter')}>
+                Wetter
+            </div>
+        )
+
 
         return (
             <>
-                <Content title={title} />
+                <div className={style.mainWrapper}>
+                    <div className={style.nav}>
+                        {elements}
+                    </div>
+                    <div>
+                        <Content title={title} />
+                    </div>
+                </div>
             </>
 
         );
