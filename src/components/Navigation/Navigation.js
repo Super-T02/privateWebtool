275e5e3f53c01935d2fa2ac43279046c2783c5b2
Super-T02 <75214791+Super-T02@users.noreply.github.com>
2021-09-03 10:15:09 +0200

    TW-8: added structure and first elements for the Navbar

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,36 @@
+import React from "react"
+import {MenuItems} from "./MenuItems";
+import style from "Navigation.module.scss"
+
+class Navigation extends React.Component {
+
+
+    render() {
+        return (
+            <>
+                <nav className={style.items}>
+                    <h1 className={style.logo}>Tom's Webtool</h1>
+                    <div className={style.menuIcon}>
+
+                    </div>
+                    <ul>
+                        {
+                            // Mapping the data from MenuItems to a list item
+                            MenuItems.map((item, index) => {
+                                return (
+                                    <li key={index}>
+                                        <a className={item.cName} href={item.url}>
+                                            {item.title}
+                                     </a>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </nav>
+            </>
+        )
+    }
+}
+
+export default Navigation;
