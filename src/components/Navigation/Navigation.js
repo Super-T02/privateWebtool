1a5022cde9c4e5a5410c32cdafa06702da72fe14
Super-T02 <75214791+Super-T02@users.noreply.github.com>
2021-09-11 14:53:25 +0200

    eddited the navbar and finished first styling

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,19 +1,24 @@
 import React from "react"
 import {MenuItems} from "./MenuItems";
 import style from "./Navigation.module.scss"
+import {Button} from "../Button";
 
 class Navigation extends React.Component {
+    state = { clicked: false }
 
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked });
+    }
 
     render() {
         return (
             <>
                 <nav className={style.items}>
-                    <h1 className={style.logo}>Tom's Webtool <i className="fab fa-react"></i></h1>
-                    <div className={style.menuIcon}>
-
+                    <h1 className={style.logo}>Tom's Webtool <i className="fab fa-react"/></h1>
+                    <div className={style.menuIcon} onClick={this.handleClick}>
+                        <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}/>
                     </div>
-                    <ul>
+                    <ul className={this.state.clicked ? style.menu + ' ' + style.active : style.menu}>
                         {
                             // Mapping the data from MenuItems to a list item
                             MenuItems.map((item, index) => {
@@ -21,12 +26,13 @@ class Navigation extends React.Component {
                                     <li key={index}>
                                         <a className={item.cName} href={item.url}>
                                             {item.title}
-                                     </a>
+                                        </a>
                                     </li>
                                 )
                             })
                         }
                     </ul>
+                    <Button>Sign Up</Button>
                 </nav>
             </>
         )
